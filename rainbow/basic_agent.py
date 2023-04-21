bedbf1abe6212761afc7ea45f5a1529b25c44335
PrzemekS <przemeksekula@gmail.com>
2023-04-21 23:38:31 +0200

    fix: rainbow folder added

---
+++ rainbow/basic_agent.py
@@ -0,0 +1,137 @@
+import numpy as np
+import random
+from collections import namedtuple, deque
+
+from rainbow.replay_buffer import ReplayBuffer
+
+from rainbow.dense_network import QNetwork
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Agent():
+    """Interacts with and learns from the environment."""
+
+    def __init__(self, state_size, action_size,
+                 buffer_size = int(1e5),
+                 batch_size = 64,
+                 gamma = 0.99,
+                 lr = 5e-4,
+                 update_every = 4,
+                 device = None):
+        """Initialize an Agent object.
+        Args:
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            buffer_size (int): replay buffer size. Default: int(1e5)
+            batch_size (int): minibatch size. Default: 64
+            gamma (float): discount factor. Default: 0.99
+            lr (float): learning rate. Default: 5e-4
+            update_every (int): how often to update the network. Default: 1        
+            device (torch.device): device to use. Default: None    
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.lr = lr
+        self.update_every = update_every
+        self.device = device
+
+        # Q-Network
+        fc1, fc2 = 64, 16 # Size of the layers
+        # TODO: Create a network with 2 hidden layers (fc1 and fc2 nodes)
+        self.qnetwork = QNetwork(state_size, action_size, fc1, fc2).to(device)
+        
+        # TODO: Create the optimizer (Adam with learning rate lr)
+        self.optimizer = optim.Adam(self.qnetwork.parameters(), lr=lr)
+
+        # TODO: Create a Replay Buffer
+        self.memory = ReplayBuffer(buffer_size)
+        
+        # Initialize time step (for updating every self.update_every steps)
+        self.t_step = 0
+    
+    def step(self, state, action, reward, next_state, done):
+        
+        # TODO: Save experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+        
+        # Learn every self.update_every time steps.
+        self.t_step = (self.t_step + 1) % self.update_every
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > self.batch_size:
+                experiences = self.memory.sample(self.batch_size)
+                self.learn(experiences)
+
+    def act(self, state, eps=0.):
+        """Returns actions for given state as per current policy.
+        
+        Params
+        ======
+            state (array_like): current state
+            eps (float): epsilon, for epsilon-greedy action selection
+        """
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.qnetwork.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork(state)
+        self.qnetwork.train()
+
+        # Epsilon-greedy action selection
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+    def learn(self, experiences):
+        """Update value parameters using given batch of experience tuples.
+
+        Params
+        ======
+            experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples 
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+        
+        device = self.device  # for shortening the following lines
+        states = torch.FloatTensor(states).to(device)
+        actions = torch.LongTensor(actions.reshape(-1, 1)).to(device)
+        rewards = torch.FloatTensor(rewards.reshape(-1, 1)).to(device)
+        next_states = torch.FloatTensor(next_states).to(device)
+        dones = torch.FloatTensor(dones.reshape(-1, 1)).to(device)        
+        
+        # Get max predicted Q values (for next states) from target model
+        Q_targets_next = self.qnetwork(next_states).detach().max(dim=1, keepdim=True)[0]
+        # TODO: Compute Q targets for current states 
+        Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
+
+        # TODO: Get expected Q values from local model
+        Q_expected = self.qnetwork(states).gather(1, actions)
+
+        # Compute loss
+        loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
+    def save(self, path = 'checkpoint.pth'):
+        """Saves the network
+        Args:
+            path (str): path to save the network. Default is 'checkpoint.pth'
+        """
+        torch.save(self.qnetwork.state_dict(), path)
+ 
+ 
+    def load(self, path = 'checkpoint.pth'):
+        """Loads the network
+        Args:
+            path (str): path with the weights. Default is 'checkpoint.pth'
+        """
+        self.qnetwork.load_state_dict(torch.load(path))
---
+++ rainbow/dense_network.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Fully connected (dense) neural network that estimates Q values."""
+
+    def __init__(self, state_size, action_size, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Args:
+            state_size (int): Dimension of each state. It equals
+                the number of features in the network.
+            action_size (int): Dimension of each action. It equals 
+                the number of the network outputs
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        
+        # TODO: Create a network with 2 hidden layers (fc1 and fc2 
+        # nodes) and output layer with action_size nodes.
+        # use nn.Linear to create the layers (no activation function)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values.
+        Args:
+            state (torch.Tensor): The state of the environment
+        Returns:
+            torch.Tensor: The action values
+        """
+        
+        # TODO: Implement the forward pass. Use ReLU activation function
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
